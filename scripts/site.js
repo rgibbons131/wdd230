f32c4a83de2781dc42361f856f6ea018c8e3d5d0
rgibbons131 <richiegibbons99@gmail.com>
2023-01-10 10:40:41 -0700

    Updates from class
    Finished the initial first website

---
+++ scripts/site.js
@@ -0,0 +1,8 @@
+// Gets todays date for the year
+const today = new Date();
+
+// Gets the year span and sets it to be the current year
+document.querySelector('#currentyear').textContent = today.getFullYear();
+
+// This sets the last modified area to the correct last modified
+document.querySelector("#lastupdated").textContent = document.lastModified;
