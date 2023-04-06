6211e13667906ce58225f22e5e081be9523cd2a8
rgibbons131 <richiegibbons99@gmail.com>
2023-04-06 17:35:38 -0600

    added resources + footer

---
+++ bountifulfoods/scripts/footer.js
@@ -0,0 +1,5 @@
+// Gets todays date for the year
+const today = new Date();
+
+// This sets the last modified area to the correct last modified
+document.querySelector("#lastupdated").textContent = document.lastModified;
